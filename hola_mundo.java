55a74da842255e6fd0151a7b6a01da9845301547
juan <130761970+olascoaga3435@users.noreply.github.com>
2023-08-14 22:44:57 +0200

    Add files via upload

---
+++ hola_mundo.java
@@ -0,0 +1,12 @@
+
+
+/*
+ *
+ * @author jose francisco
+ */
+public class hola_mundo {
+    public static void main(String[]args){
+        System.out.println("hola jose francisco");
+    }
+    
+}
