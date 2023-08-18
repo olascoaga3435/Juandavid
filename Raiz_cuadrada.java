1368ba4910d623258a52470e4ce4e9442895420b
juan <130761970+olascoaga3435@users.noreply.github.com>
2023-08-18 06:34:21 -0500

    Create Raiz_cuadrada.java

---
+++ Raiz_cuadrada.java
@@ -0,0 +1,8 @@
+public class Raiz_cuadrada {
+    public static void main(String[] args) {
+        double numero = 10.0;
+        double raizCuadrada = Math.sqrt(numero);
+
+        System.out.println("La raíz cuadrada de " + numero + " es: " + raizCuadrada);
+    }
+}
